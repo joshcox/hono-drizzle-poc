ea943d75814fa78e2bf3a183409a80932162990b
Josh Cox <josh.cox@hingehealth.com>
2024-08-05 14:16:02 +0000

    Add additional content type to route

--- src/user/app/get.user/get.user.route.ts
+++ src/user/app/get.user/get.user.route.ts
@@ -1,6 +1,7 @@
 import { createRoute, z } from "@hono/zod-openapi";
 import getUserQuery from "./get.user.query";
 import app from "../../../app";
+import UserSchema from "../../domain/user";
 
 export default app.openapi(
   createRoute({
@@ -11,13 +12,23 @@ export default app.openapi(
         description: 'User',
         content: {
           'application/json': {
-            schema: z.object({
-              name: z.string(),
-            }),
+            schema: UserSchema,
+          },
+          'text/html': {
+            schema: z.string(),
           },
         },
       },
     },
   }),
-  (c) => c.json(getUserQuery())
+  async (c) => {
+    const user = await getUserQuery();
+    const acceptHeader = c.req.header('Accept');
+
+    if (acceptHeader && acceptHeader.includes('text/html')) {
+      return c.html(`<h1>User</h1><p>Name: ${user.name}</p>`) as any;
+    } else {
+      return c.json(user);
+    }
+  }
 );
